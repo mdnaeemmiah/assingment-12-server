19983b73b04e5cccb0bedf05c67200069feb86be
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2024-10-28 18:07:13 +0600

    contest added

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const jwt = require('jsonwebtoken')
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 8000
+
+// 'https://user-email-password-24983.web.app', 'https://user-email-password-24983.firebaseapp.com'
 
 // middleware
 const corsOptions = {
@@ -49,7 +51,9 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const usersCollection = client.db('ContestCreation').collection('users');
-   
+    const contestsCollection = client.db('ContestCreation').collection('contest');
+    const bookingsCollection = client.db('ContestCreation').collection('booking');
+
 
     // auth related api
     app.post('/jwt', async (req, res) => {
@@ -81,7 +85,26 @@ async function run() {
       }
     })
 
-    
+
+    // create-payment-intent
+    app.post('/create-payment-intent', async (req, res) => {
+      const price = req.body.price
+      const priceInCent = parseFloat(price) * 100
+      if (!price || priceInCent < 1) return
+      // generate clientSecret
+      const { client_secret } = await stripe.paymentIntents.create({
+        amount: priceInCent,
+        currency: 'usd',
+        // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      })
+      // send client secret as response
+      res.send({ clientSecret: client_secret })
+    })
+
+
     // save a user data in db
     app.put('/user', async (req, res) => {
       const user = req.body
@@ -114,6 +137,135 @@ async function run() {
       res.send(result)
     })
 
+
+
+    // get a user info by email from db
+    app.get('/user/:email', async (req, res) => {
+      const email = req.params.email
+      const result = await usersCollection.findOne({ email })
+      res.send(result)
+    })
+
+    // get all users data from db
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray()
+      res.send(result)
+    })
+
+    //update a user role
+    app.patch('/users/update/:email', async (req, res) => {
+      const email = req.params.email
+      const user = req.body
+      const query = { email }
+      const updateDoc = {
+        $set: { ...user, timestamp: Date.now() },
+      }
+      const result = await usersCollection.updateOne(query, updateDoc)
+      res.send(result)
+    })
+
+    // Get all contest from db
+    app.get('/contests', async (req, res) => {
+      const category = req.query.category
+      console.log(category)
+      let query = {}
+      if (category && category !== 'null') query = { category }
+      const result = await contestsCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    // Save a contest data in db
+    app.post('/contest', async (req, res) => {
+      const contestData = req.body
+      const result = await contestsCollection.insertOne(contestData)
+      res.send(result)
+    })
+
+    // get all contests for host
+    app.get('/my-contest/:email', async (req, res) => {
+      const email = req.params.email
+
+      let query = { 'host.email': email }
+      const result = await contestsCollection.find(query).toArray()
+      res.send(result)
+    }
+    )
+
+    // delete a contest
+    app.delete('/contest/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await contestsCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    // Get a single contest data from db using _id
+    app.get('/contest/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await contestsCollection.findOne(query)
+      res.send(result)
+    })
+
+    // Save a contest data in db
+    app.post('/booking', async (req, res) => {
+      const bookingData = req.body
+      // save room booking info
+      const result = await bookingsCollection.insertOne(bookingData)
+      // send email to guest
+      // sendEmail(bookingData?.guest?.email, {
+      //   subject: 'Booking Successful!',
+      //   message: `You've successfully booked a room through StayVista. Transaction Id: ${bookingData.transactionId}`,
+      // })
+      // send email to host
+      // sendEmail(bookingData?.host?.email, {
+      //   subject: 'Your room got booked!',
+      //   message: `Get ready to welcome ${bookingData.guest.name}.`,
+      // })
+
+      res.send(result)
+    })
+
+    // update room data
+    // app.put('/room/update/:id', async (req, res) => {
+    //   const id = req.params.id
+    //   const roomData = req.body
+    //   const query = { _id: new ObjectId(id) }
+    //   const updateDoc = {
+    //     $set: roomData,
+    //   }
+    //   const result = await roomsCollection.updateOne(query, updateDoc)
+    //   res.send(result)
+    // })
+
+    // get all booking for a guest
+    app.get('/my-contests/:email', verifyToken, async (req, res) => {
+      const email = req.params.email
+      const query = { 'guest.email': email }
+      const result = await bookingsCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    // get all booking for a host
+    // app.get('/manage-bookings/:email',
+    //   async (req, res) => {
+    //     const email = req.params.email
+    //     const query = { 'host.email': email }
+    //     const result = await bookingsCollection.find(query).toArray()
+    //     res.send(result)
+    //   }
+    // )
+
+    // delete a booking
+    app.delete('/booking/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await bookingsCollection.deleteOne(query)
+      res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
@@ -126,9 +278,9 @@ async function run() {
 run().catch(console.dir)
 
 app.get('/', (req, res) => {
-  res.send('Hello from StayVista Server..')
+  res.send('Hello from Contest Creation Platform Server..')
 })
 
 app.listen(port, () => {
-  console.log(`StayVista is running on port ${port}`)
+  console.log(`Contest Creation Platform is running on port ${port}`)
 })
